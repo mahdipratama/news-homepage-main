94d4d1989514a4fe6d35792784090152ef56aec4
mahdipratama <mahdipratama29@gmail.com>
2022-12-09 08:53:45 +0700

    create overlay for active menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,21 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav__menu');
 const closeHamburger = document.querySelector('.close-hamburger');
+const overlay = document.querySelector('.overlay');
+
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   closeHamburger.classList.toggle('active');
+  overlay.classList.toggle('active');
 })
 
 closeHamburger.addEventListener('click', () => {
   navMenu.classList.remove('active');
   closeHamburger.classList.remove('active');
   hamburger.classList.remove('active');
+  overlay.classList.remove('active');
 })
 
 document.querySelectorAll('.nav__list').forEach(
@@ -20,6 +24,7 @@ document.querySelectorAll('.nav__list').forEach(
       hamburger.classList.remove('active');
       navMenu.classList.remove('active');
       closeHamburger.classList.remove('active');
+      overlay.classList.remove('active');
     })
   })
 
